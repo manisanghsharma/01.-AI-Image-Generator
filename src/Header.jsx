7bb76f84ddefb309a8b64159019772ed1a91b27c
Manisangh Sharma <manisanghsharma@gmail.com>
2023-11-11 22:42:05 +0530

    img

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,11 @@ const Header = () => {
     const githubUrl = "https://www.github.com/manisanghsharma";
     return (
         <header className="header">
-            <img id="robot" src="./src/robot.png" alt="" />
+            <img
+                id="robot"
+                src="https://i.ibb.co/ZgmMFPx/0n8bbimljc2crngf9177hq2o6u.png"
+                alt=""
+            />
             <h1 id="title">AI Image Generator</h1>
             <button id="github" onClick={() => window.open(githubUrl)}>
                 <FaGithub id="git-icon" />
